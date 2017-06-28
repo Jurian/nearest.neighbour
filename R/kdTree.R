4bb40972c07adfaa62721108c64f5d8944d8d5b8
Baas <baasj@pc160095.knmi.nl>
2017-06-28 13:38:26 +0000

    Started on supporting partially duplicate observations

--- R/kdTree.R
+++ R/kdTree.R
@@ -2,7 +2,7 @@
 #' @title Construct a KD-Tree using only basic vanilla R functionality
 #' @description This algorithm builds a KD-Tree. NB currently duplicate values can cause undefined behaviour!
 #' @param X Data frame of values on which to perform splits
-#' @param y Vector or data frame with data that should be stored in the leafs
+#' @param y Vector with data that should be stored in the leafs
 #' @param FUN Optional function to use for determining position of split. Defaults to median as this gives a balanced tree
 #' @param ... Optional arguments to pass on to FUN
 #' @author Jurian Baas
@@ -22,13 +22,14 @@ buildKdTree <- function(X, y, FUN = median, ...) {
     split <- FUN(X[[i]], ...)
     idx.1 <- X[[i]] <= split
     idx.2 <- !idx.1
-
+    
     tree  <- c(tree, list(
       value = split,
-      dim = i,
-      left = build(X[idx.1], y[idx.1], depth + 1, tree, FUN),
-      right = build(X[idx.2], y[idx.2], depth + 1, tree, FUN)
+      dim = i
     ))
+    
+    if(sum(idx.1) > 0) tree$left <- build(X[idx.1], y[idx.1], depth + 1, tree, FUN)
+    if(sum(idx.2) > 0) tree$right <- build(X[idx.2], y[idx.2], depth + 1, tree, FUN)
 
     return(tree)
 
@@ -47,68 +48,69 @@ buildKdTree <- function(X, y, FUN = median, ...) {
 
 #' @title Construct a KD-Tree using the data.tree package
 #' @description This algorithm works similarly as buildKdTree but allows for more fancy operation such as child-parent traversal
-#' @param X Data frame of values on which to perform splits
-#' @param y Vector or data frame with data that should be stored in the leafs
+#' @param X Data table of values on which to perform splits
+#' @param y Vector with data that should be stored in the leafs
+#' @param scale Should data be scaled beforehand, defaults to true
 #' @param FUN Optional function to use for determining position of split. Defaults to median as this gives a balanced tree
 #' @param ... Optional arguments to pass on to FUN
 #' @author Jurian Baas
 #' @export
 #' @importFrom data.tree Node
 #' @importFrom stats median
-buildKdDataTree <- function(X, y, FUN = median, ...) {
+#' @importFrom data.table data.table
+buildKdDataTree <- function(X, y, scale = TRUE, FUN = median, ...) {
 
-  if(!is.data.frame(X)) stop("First argument has to be a data frame")
-
+  if(scale) X <- data.table::data.table(scale(X))
+  
   # Nr of dimensions
   d <- ncol(X)
 
-  build <- function(X, y, depth, parent, side, FUN) {
+  build <- function(X, y, depth, parent, FUN) {
 
     if(nrow(X) == 1) {
-      y <- y[1,]
+  
       leaf <- parent$AddChild(
         paste(y, collapse = ","),
-        value = y,
-        side = side
+        value = y
       )
 
     } else {
 
+      # Iterate over dimensions
       i <- depth %% d + 1
-
+      
+      # compute split
       split <- FUN(X[[i]], ...)
       idx.1 <- X[[i]] <= split
       idx.2 <- !idx.1
-
+      
+      node.name <- paste(colnames(X)[i], "=", split)
+      
       if(is.null(parent)) {
-        root <- data.tree::Node$new(
-          as.character(split),
+        node <- data.tree::Node$new (
+          node.name,
           split = split,
-          dim = i,
-          side = 'root'
+          dim = i
         )
-
-        build(X[idx.1], y[idx.1], depth + 1, root, 'left', FUN)
-        build(X[idx.2], y[idx.2], depth + 1, root, 'right', FUN)
-
-        return(root)
-
       } else {
-        child <- parent$AddChild(
-          as.character(split),
+        node <- parent$AddChild (
+          node.name,
           split = split,
-          dim = i,
-          side = side
+          dim = i
         )
-
-        build(X[idx.1], y[idx.1], depth + 1, child, 'left', FUN)
-        build(X[idx.2], y[idx.2], depth + 1, child, 'right', FUN)
       }
+      
+      # Build subtree recursively
+      build(X[idx.1], y[idx.1], depth + 1, node, FUN)
+      build(X[idx.2], y[idx.2], depth + 1, node, FUN)
+      
+      if(is.null(parent)) return(node)
+      
     }
   }
 
   kd <- list()
-  kd$tree <- build(X, y, depth = 0, parent = NULL, side = NULL, FUN = FUN)
+  kd$tree <- build(X, y, depth = 0, parent = NULL, FUN = FUN)
   kd$X = X
   kd$dim <- d
 
@@ -116,6 +118,15 @@ buildKdDataTree <- function(X, y, FUN = median, ...) {
 
   return(kd)
 
+}
+
+#' @title Composite of an n-dimensional point
+#' @description Calculate the associated point in the composite number space
+#' @param p Point to calculate composite of
+#' @return The composite of the given point
+#' @author Jurian Baas
+composite <- function(p) {
+  return(c(as.numeric(paste0(p, collapse = '')),as.numeric(paste0(rev(p), collapse = ''))))
 }
 
 #' @title Plot a scatter plot or a tree representation of a two dimensional KD-Tree
@@ -131,10 +142,11 @@ plot.kd.data.tree <- function(x, ..., plotting.type = "scatter") {
   kdtree <- x
 
   if(class(kdtree) != "kd.data.tree") stop("Parameter is not of class kd.data.tree")
-  if(kdtree$dim != 2) stop("Currently only two dimensional trees can be plotted")
 
   if(plotting.type == "scatter") {
-
+    
+    if(kdtree$dim != 2) stop("Currently only two dimensional trees can be plotted")
+    
     graphics::plot(kdtree$X,
                    col= "blue", pch = 19, cex = 1, lty = "solid", lwd = 2)
     graphics::text(kdtree$X,
@@ -145,38 +157,7 @@ plot.kd.data.tree <- function(x, ..., plotting.type = "scatter") {
 
     kdtree$tree$Do(function(node) {
       if(!node$isLeaf) {
-
-        # If it's a vertical line
-        if(node$dim == 1) {
-
-          r <- range(box[3:4])
-
-          if(node$side == 'left') {
-            r[2] <- node$parent$split
-          } else if(node$side == 'right') {
-            r[1] <- node$parent$split
-          }
-
-          graphics::lines(
-            x = c(node$split, node$split),
-            y = r
-          )
-        } else {
-
-          r <- range(box[1:2])
-
-          if(node$side == 'left') {
-            r[2] <- node$parent$split
-          } else if(node$side == 'right') {
-            r[1] <- node$parent$split
-          }
-
-          graphics::lines(
-            y = c(node$split, node$split),
-            x = r
-          )
-        }
-
+        
       }
     })
 
